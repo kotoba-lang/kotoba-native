502f2921c7016b6c3772ca2ea0069b68e28d0b17
Jun Kawasaki <04.feasts_minded@icloud.com>
2026-07-27 00:43:18 +0900

    add direct native emitter coverage (#1)

--- test/kotoba/native_test.clj
+++ test/kotoba/native_test.clj
@@ -1,7 +1,7 @@
 (ns kotoba.native-test
   (:require [clojure.test :refer [deftest is]]
-            [kotoba.native.x86-64]
-            [kotoba.native.aarch64]
+            [kotoba.native.x86-64 :as x86]
+            [kotoba.native.aarch64 :as arm]
             [kotoba.native.elf64]))
 
 ;; Load gate: the split must not break namespace resolution. Each extracted
@@ -10,3 +10,15 @@
   (is (some? (find-ns 'kotoba.native.x86-64)) "kotoba.native.x86-64 must load")
   (is (some? (find-ns 'kotoba.native.aarch64)) "kotoba.native.aarch64 must load")
   (is (some? (find-ns 'kotoba.native.elf64)) "kotoba.native.elf64 must load"))
+
+(deftest native-backends-deterministically-own-code-and-export-layout
+  (let [kir {:format :kotoba.kir/v4 :exports ['main]
+             :functions [{:name 'main :params [] :body 42}]}
+        x (x86/emit-program kir)
+        a (arm/emit-program kir)]
+    (is (= x (x86/emit-program kir)))
+    (is (= a (arm/emit-program kir)))
+    (is (seq (:code x)))
+    (is (seq (:code a)))
+    (is (= 0 (get-in x [:exports 'main :offset])))
+    (is (= 0 (get-in a [:exports 'main :offset])))))
